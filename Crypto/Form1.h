a558d4ccd22545e15ebfc5fa38c45c5df35bab54
N.Chandra Kanth <canfindck@gmail.com>
2016-07-19 15:05:12 +0530

    Commit
    New

--- Crypto/Form1.h
+++ Crypto/Form1.h
@@ -11,8 +11,9 @@ int value=0;
 #define RECEIVE_IP          "127.0.0.1"
 unsigned short ReceivingPort = 8005;
 
-UdpInSocket inSocket_Ownship1(RECEIVE_IP,  ReceivingPort);    
+UdpInSocket inSocket_Ownship1(RECEIVE_IP,  ReceivingPort);
 
+UdpOutSocket outSocket_Ownship2(RECEIVE_IP,  ReceivingPort);
 
 namespace Crypto {
 
@@ -136,8 +137,22 @@ namespace Crypto {
 		array<Byte>^ cdata;
 		array<Byte>^ rbytes;
 		array<Byte>^ descipher;
+		array<Byte>^ signcipher;
 		SymmetricAlgorithm^ desObj;
 		RSACryptoServiceProvider^ rsaObj;
+
+	private: System::Windows::Forms::TextBox^  textBox3;
+	public: 
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Button^  button3;
+	private: Microsoft::VisualBasic::PowerPacks::ShapeContainer^  shapeContainer1;
+	private: Microsoft::VisualBasic::PowerPacks::LineShape^  lineShape2;
+	private: Microsoft::VisualBasic::PowerPacks::LineShape^  lineShape1;
+
+
+
+
 
 
 	private: System::Windows::Forms::PictureBox^  pictureBox1;
@@ -205,6 +220,13 @@ namespace Crypto {
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->shapeContainer1 = (gcnew Microsoft::VisualBasic::PowerPacks::ShapeContainer());
+			this->lineShape2 = (gcnew Microsoft::VisualBasic::PowerPacks::LineShape());
+			this->lineShape1 = (gcnew Microsoft::VisualBasic::PowerPacks::LineShape());
 			this->groupBox1->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
@@ -213,7 +235,7 @@ namespace Crypto {
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(92, 401);
+			this->button1->Location = System::Drawing::Point(103, 478);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(154, 58);
 			this->button1->TabIndex = 0;
@@ -225,7 +247,7 @@ namespace Crypto {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(580, 401);
+			this->button2->Location = System::Drawing::Point(580, 478);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(152, 58);
 			this->button2->TabIndex = 1;
@@ -238,7 +260,7 @@ namespace Crypto {
 			this->textBox1->BackColor = System::Drawing::SystemColors::Window;
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Georgia", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(217, 41);
+			this->textBox1->Location = System::Drawing::Point(217, 39);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(557, 59);
@@ -250,7 +272,7 @@ namespace Crypto {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Georgia", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(28, 60);
+			this->label1->Location = System::Drawing::Point(28, 58);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(127, 16);
 			this->label1->TabIndex = 3;
@@ -263,7 +285,7 @@ namespace Crypto {
 			this->groupBox1->Controls->Add(this->radioButton1);
 			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Georgia", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->groupBox1->Location = System::Drawing::Point(312, 124);
+			this->groupBox1->Location = System::Drawing::Point(317, 118);
 			this->groupBox1->Name = L"groupBox1";
 			this->groupBox1->Size = System::Drawing::Size(383, 145);
 			this->groupBox1->TabIndex = 4;
@@ -310,7 +332,7 @@ namespace Crypto {
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Georgia", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(217, 294);
+			this->textBox2->Location = System::Drawing::Point(217, 379);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(557, 59);
@@ -321,7 +343,7 @@ namespace Crypto {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Georgia", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(20, 315);
+			this->label2->Location = System::Drawing::Point(20, 401);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(135, 16);
 			this->label2->TabIndex = 6;
@@ -330,17 +352,83 @@ namespace Crypto {
 			// pictureBox1
 			// 
 			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^  >(resources->GetObject(L"pictureBox1.Image")));
-			this->pictureBox1->Location = System::Drawing::Point(103, 137);
+			this->pictureBox1->Location = System::Drawing::Point(27, 159);
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->Size = System::Drawing::Size(128, 130);
 			this->pictureBox1->TabIndex = 7;
 			this->pictureBox1->TabStop = false;
 			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(217, 306);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(136, 47);
+			this->textBox3->TabIndex = 8;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(638, 305);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(136, 47);
+			this->textBox4->TabIndex = 9;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(247, 282);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(72, 14);
+			this->label3->TabIndex = 10;
+			this->label3->Text = L"Signature :";
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(456, 305);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 47);
+			this->button3->TabIndex = 11;
+			this->button3->Text = L"Encrypt";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &Form1::button3_Click);
+			// 
+			// shapeContainer1
+			// 
+			this->shapeContainer1->Location = System::Drawing::Point(0, 0);
+			this->shapeContainer1->Margin = System::Windows::Forms::Padding(0);
+			this->shapeContainer1->Name = L"shapeContainer1";
+			this->shapeContainer1->Shapes->AddRange(gcnew cli::array< Microsoft::VisualBasic::PowerPacks::Shape^  >(2) {this->lineShape2, 
+				this->lineShape1});
+			this->shapeContainer1->Size = System::Drawing::Size(840, 554);
+			this->shapeContainer1->TabIndex = 12;
+			this->shapeContainer1->TabStop = false;
+			// 
+			// lineShape2
+			// 
+			this->lineShape2->Name = L"lineShape2";
+			this->lineShape2->X1 = 359;
+			this->lineShape2->X2 = 446;
+			this->lineShape2->Y1 = 326;
+			this->lineShape2->Y2 = 326;
+			// 
+			// lineShape1
+			// 
+			this->lineShape1->Name = L"lineShape1";
+			this->lineShape1->X1 = 538;
+			this->lineShape1->X2 = 625;
+			this->lineShape1->Y1 = 326;
+			this->lineShape1->Y2 = 326;
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(7, 14);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(813, 490);
+			this->ClientSize = System::Drawing::Size(840, 554);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox2);
@@ -349,6 +437,7 @@ namespace Crypto {
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
+			this->Controls->Add(this->shapeContainer1);
 			this->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
 			this->Name = L"Form1";
@@ -421,26 +510,29 @@ namespace Crypto {
 			 }
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) 
 		 {
+			 StreamWriter^ outfile = gcnew StreamWriter("C:\\Users\\Administrator.MCS-337\\Desktop\\All\\Crypto\\Crypto\\Crypto\\Intermi.txt");
+			 outfile->Write(textBox2->Text); 
+			 outfile->Close();
 
 			 if (value==1)
 			 {
 				MessageBox::Show("Message Sent Sucessfully..!!!");
 				this->Hide();
-				Form2^ f2 = gcnew Form2(textBox2->Text, cipherbytes, rbytes, desObj, textBox1->Text, value, Final);
+				Form2^ f2 = gcnew Form2(textBox2->Text, cipherbytes, rbytes, desObj, textBox1->Text, value, Final, signcipher);
 				f2->ShowDialog();
 			 }
 			 else if (value == 2)
 			 {
-				 MessageBox::Show("Message Sent Sucessfully..!!!");
+				MessageBox::Show("Message Sent Sucessfully..!!!");
 				this->Hide();
-				Form2^ f2 = gcnew Form2(textBox2->Text, cipherbytes, rbytes, desObj, textBox1->Text, value, Final);
+				Form2^ f2 = gcnew Form2(textBox2->Text, cipherbytes, rbytes, desObj, textBox1->Text, value, Final, signcipher);
 				f2->ShowDialog();
 			 }
 			 else
 			 {
 				MessageBox::Show("Message Sent Sucessfully..!!!");
 				this->Hide();
-				Form2^ f2 = gcnew Form2(textBox2->Text, cipherbytes, rbytes, desObj, textBox1->Text, value, Final);
+				Form2^ f2 = gcnew Form2(textBox2->Text, cipherbytes, rbytes, desObj, textBox1->Text, value, Final, signcipher);
 				f2->ShowDialog();
 			 }
 		 }
@@ -460,5 +552,20 @@ namespace Crypto {
 		 value=3;
 		 }
 
+private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
+			 String^ sign = textBox3->Text;
+			 DESCryptoServiceProvider^ DesAlg = gcnew DESCryptoServiceProvider;
+			
+			 signcipher = EncryptTextToMemory(sign, DesAlg->Key, DesAlg->IV);
+			 textBox4->Text = Encoding::ASCII->GetString(signcipher);
+
+			 StreamWriter^ outfile = gcnew StreamWriter("C:\\Users\\Administrator.MCS-337\\Desktop\\All\\Crypto\\Crypto\\Crypto\\Signature.txt");
+			 outfile->Write(textBox4->Text);
+			 outfile->Close();
+
+		 }
+private: System::Void menuStrip1_ItemClicked(System::Object^  sender, System::Windows::Forms::ToolStripItemClickedEventArgs^  e) {
+			 Application::Exit();
+		 }
 };
 }
--- Crypto/Form2.h
+++ Crypto/Form2.h
@@ -14,10 +14,28 @@ namespace Crypto {
 	using namespace System::IO;
 	using namespace System::Security::Cryptography;
 	using namespace System::Globalization;
+	using namespace System::Collections;
 
-	/// <summary>
-	/// Summary for Form2
-	/// </summary>
+	static array<Byte>^ DecryptTextMemory( array<Byte> ^Data2, array<Byte> ^Key2, array<Byte> ^IV)
+	{
+		try
+			{
+
+				 MemoryStream^ msDecryp = gcnew MemoryStream( Data2 );
+				 //Creating a new instance of CryptoStream Memory.
+				 CryptoStream ^csDecryp = gcnew CryptoStream( msDecryp,(gcnew DESCryptoServiceProvider)->CreateDecryptor( Key2, IV ),CryptoStreamMode::Read );
+				 array<Byte> ^fromEncryp = gcnew array<Byte>(Data2->Length);
+
+				 csDecryp->Read( fromEncryp, 0, fromEncryp->Length );
+				 return fromEncryp;
+			}
+		catch ( CryptographicException^ e ) 
+			{
+				 Console::WriteLine( "A Cryptographic error occurred: {0}", e->Message );
+				 return nullptr;
+			}
+	}
+
 	public ref class Form2 : public System::Windows::Forms::Form
 	{
 
@@ -31,13 +49,17 @@ namespace Crypto {
 		SymmetricAlgorithm^ dObj;
 
 	private: System::Windows::Forms::TextBox^  textBox1;
-	private: System::Windows::Forms::TextBox^  textBox3;
-	private: System::Windows::Forms::Label^  label3;
+
+
 	private: System::Windows::Forms::Button^  button5;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::Button^  button6;
 	public: 
 
 	public: 
 		String ^main;
+		String ^falstr;
 
 	public:
 		Form2(void)
@@ -45,7 +67,7 @@ namespace Crypto {
 			InitializeComponent();
 		}
 
-		Form2(String ^Text, array<Byte> ^cipher, array<Byte> ^rbytes, SymmetricAlgorithm^ desObj, String ^Text2, int vv, array<Byte>^ Final)
+		Form2(String ^Text, array<Byte> ^cipher, array<Byte> ^rbytes, SymmetricAlgorithm^ desObj, String ^Text2, int vv, array<Byte>^ Final, array<Byte>^ signcipher)
 		{
 			InitializeComponent();
 			word = Text;
@@ -103,9 +125,10 @@ namespace Crypto {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
-			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
-			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->button6 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// label2
@@ -113,17 +136,18 @@ namespace Crypto {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Georgia", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(31, 310);
+			this->label2->Location = System::Drawing::Point(29, 402);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(119, 16);
 			this->label2->TabIndex = 10;
 			this->label2->Text = L"Original Message :";
+			this->label2->Click += gcnew System::EventHandler(this, &Form2::label2_Click);
 			// 
 			// textBox2
 			// 
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Georgia", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->textBox2->Location = System::Drawing::Point(200, 290);
+			this->textBox2->Location = System::Drawing::Point(217, 379);
 			this->textBox2->Multiline = true;
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(557, 59);
@@ -134,7 +158,7 @@ namespace Crypto {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Georgia", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(30, 59);
+			this->label1->Location = System::Drawing::Point(28, 58);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(120, 16);
 			this->label1->TabIndex = 8;
@@ -144,7 +168,7 @@ namespace Crypto {
 			// 
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Georgia", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->textBox1->Location = System::Drawing::Point(200, 41);
+			this->textBox1->Location = System::Drawing::Point(217, 39);
 			this->textBox1->Multiline = true;
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(557, 60);
@@ -156,7 +180,7 @@ namespace Crypto {
 			this->button1->Enabled = false;
 			this->button1->Font = (gcnew System::Drawing::Font(L"Georgia", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(544, 162);
+			this->button1->Location = System::Drawing::Point(386, 297);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(213, 62);
 			this->button1->TabIndex = 11;
@@ -168,7 +192,7 @@ namespace Crypto {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(333, 426);
+			this->button2->Location = System::Drawing::Point(348, 498);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(163, 43);
 			this->button2->TabIndex = 12;
@@ -180,7 +204,7 @@ namespace Crypto {
 			// 
 			this->button3->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button3->Location = System::Drawing::Point(-1, 427);
+			this->button3->Location = System::Drawing::Point(14, 499);
 			this->button3->Name = L"button3";
 			this->button3->Size = System::Drawing::Size(170, 43);
 			this->button3->TabIndex = 13;
@@ -192,7 +216,7 @@ namespace Crypto {
 			// 
 			this->button4->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button4->Location = System::Drawing::Point(645, 426);
+			this->button4->Location = System::Drawing::Point(660, 498);
 			this->button4->Name = L"button4";
 			this->button4->Size = System::Drawing::Size(168, 43);
 			this->button4->TabIndex = 14;
@@ -200,47 +224,58 @@ namespace Crypto {
 			this->button4->UseVisualStyleBackColor = true;
 			this->button4->Click += gcnew System::EventHandler(this, &Form2::button4_Click);
 			// 
-			// textBox3
-			// 
-			this->textBox3->Font = (gcnew System::Drawing::Font(L"Georgia", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
-				static_cast<System::Byte>(0)));
-			this->textBox3->Location = System::Drawing::Point(200, 162);
-			this->textBox3->Multiline = true;
-			this->textBox3->Name = L"textBox3";
-			this->textBox3->Size = System::Drawing::Size(140, 62);
-			this->textBox3->TabIndex = 15;
-			// 
-			// label3
-			// 
-			this->label3->AutoSize = true;
-			this->label3->Font = (gcnew System::Drawing::Font(L"Georgia", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
-				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(232, 131);
-			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(81, 18);
-			this->label3->TabIndex = 16;
-			this->label3->Text = L"Signature :";
-			// 
 			// button5
 			// 
-			this->button5->Font = (gcnew System::Drawing::Font(L"Georgia", 9, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+			this->button5->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
 				static_cast<System::Byte>(0)));
-			this->button5->Location = System::Drawing::Point(400, 162);
+			this->button5->Location = System::Drawing::Point(540, 149);
 			this->button5->Name = L"button5";
-			this->button5->Size = System::Drawing::Size(89, 62);
+			this->button5->Size = System::Drawing::Size(96, 47);
 			this->button5->TabIndex = 17;
 			this->button5->Text = L"VERIFY";
 			this->button5->UseVisualStyleBackColor = true;
 			this->button5->Click += gcnew System::EventHandler(this, &Form2::button5_Click);
 			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(359, 149);
+			this->textBox4->Multiline = true;
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(136, 47);
+			this->textBox4->TabIndex = 18;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->label3->Location = System::Drawing::Point(394, 125);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(72, 14);
+			this->label3->TabIndex = 19;
+			this->label3->Text = L"Signature :";
+			// 
+			// button6
+			// 
+			this->button6->Enabled = false;
+			this->button6->Font = (gcnew System::Drawing::Font(L"Georgia", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->button6->Location = System::Drawing::Point(359, 236);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(277, 30);
+			this->button6->TabIndex = 20;
+			this->button6->Text = L"Verify Hashing";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
 			// Form2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(813, 487);
-			this->Controls->Add(this->button5);
+			this->ClientSize = System::Drawing::Size(841, 555);
+			this->Controls->Add(this->button6);
 			this->Controls->Add(this->label3);
-			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->button5);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
@@ -259,7 +294,12 @@ namespace Crypto {
 #pragma endregion
 
 	private: System::Void Form2_Load(System::Object^  sender, System::EventArgs^  e) {
-					textBox1->Text = word;
+					//textBox1->Text = word;
+					StreamReader^ infile = gcnew StreamReader("C:\\Users\\Administrator.MCS-337\\Desktop\\All\\Crypto\\Crypto\\Crypto\\Intermi.txt");
+					String^ rd = infile->ReadLine();
+					textBox1->Text = rd;
+					infile->Close();
+			
 			 }
 
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
@@ -286,7 +326,7 @@ namespace Crypto {
 			 }
 
 	private: System::Void button2_Click(System::Object^  sender, System::EventArgs^  e) {
-				 StreamWriter^ outfile = gcnew StreamWriter("C:\\Users\\Administrator.MCS-337\\Desktop\\Crypto\\Crypto\\Text_files\\EntireData.txt");
+				 StreamWriter^ outfile = gcnew StreamWriter("C:\\Users\\Administrator.MCS-337\\Desktop\\All\\Crypto\\Crypto\\Text_files\\EntireData.txt");
 				 outfile->Write("Original Message : " + main + "\r\n\r\n\r\n"); 
 				 outfile->Write("Encrypted Code : " + word + "\r\n\r\n\r\n");
 				 outfile->Write("Decrypted Code : " + textBox2->Text);
@@ -295,7 +335,7 @@ namespace Crypto {
 			 }
 
 	private: System::Void button3_Click(System::Object^  sender, System::EventArgs^  e) {
-				 StreamWriter^ outfile2 = gcnew StreamWriter("C:\\Users\\Administrator.MCS-337\\Desktop\\Crypto\\Crypto\\Text_files\\CipheredData.txt");
+				 StreamWriter^ outfile2 = gcnew StreamWriter("C:\\Users\\Administrator.MCS-337\\Desktop\\All\\Crypto\\Crypto\\Text_files\\CipheredData.txt");
 				 outfile2->Write("Encrypted Code : " + word + "\r\n\r\n\r\n");
 				 outfile2->Close();
 				 MessageBox::Show("The Encrypted Data saved in a Text File");
@@ -307,31 +347,50 @@ namespace Crypto {
 		 }
 
 	private: System::Void button5_Click(System::Object^  sender, System::EventArgs^  e) {
-				String^ sign = textBox3->Text;
 				
+				String ^sign = textBox4->Text;
+
+				DESCryptoServiceProvider^ DesAlg2 = gcnew DESCryptoServiceProvider;
+				StreamReader^ infile = gcnew StreamReader("C:\\Users\\Administrator.MCS-337\\Desktop\\All\\Crypto\\Crypto\\Crypto\\Signature.txt");
+				String^ rd = infile->ReadLine();
+				//MessageBox::Show(rd);
+				array<Byte> ^plbytes = Encoding::ASCII->GetBytes(rd);
+				array<Byte> ^Fal = DecryptTextMemory(plbytes, DesAlg2->Key, DesAlg2->IV);
+				falstr = Encoding::ASCII->GetString(Fal);
+
+
 				if(hi == 1)
 				{
-					if(sign == "AES")
+					if(sign == falstr) {
 						button1->Enabled = true;
+						button6->Enabled = true;
+					}
 					else
 						MessageBox::Show("Hashing Error", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 				}
 
 				if(hi == 2)
 				{
-					if(sign == "DES")
+					if(sign == falstr) {
 						button1->Enabled = true;
+						button6->Enabled = true;
+					}
 					else
 						MessageBox::Show("Hashing Error", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 				}
 
 				if(hi == 3)
 				{
-					if(sign == "RSA")
+					if(sign == falstr) {
 						button1->Enabled = true;
+						button6->Enabled = true;
+					}
 					else
 						MessageBox::Show("Hashing Error", "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
 				}
+		 }
+
+private: System::Void label2_Click(System::Object^  sender, System::EventArgs^  e) {
 		 }
 };
 }
